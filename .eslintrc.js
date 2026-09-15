8c1b06ca14862686770992e376b5885569fb05c6
Saloni <kumarisaloni2k1@gmail.com>
2026-09-15 12:54:59 +0530

    chnaged  xwalk cells to 5

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,10 @@ module.exports = {
     'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param
+
+    'xwalk/max-cells': ['error', {
+      '*': 4, // Leaves the standard default limit at 4 cells for other blocks
+      article: 5, // Permits exactly 5 cells specifically for your article block
+    }],
   },
 };
